1dcce5e02cdd8298e6c37a1d9a9e5f28e067430c
Nghia Hoang <nghiahoang880@gmail.com>
2026-03-19 23:57:30 +0700

    chore: update app view

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-function App(): React.FC {
-  return <>App</>;
-}
+import React from "react";
+import Toolbar from "./components/Toolbar";
+import Canvas from "./components/Canvas";
+
+const App: React.FC = () => {
+  return (
+    <div className="h-screen w-screen overflow-hidden bg-gray-50">
+      <Canvas />
+      <Toolbar />
+    </div>
+  );
+};
 
 export default App;
